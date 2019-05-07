3179012b470fd8413bcbfcb8ee24b1629f6a29e7
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2019-05-07 08:57:07 -0400

    stuck with react error

--- src/components/TidesDisplay/TidesDisplay.js
+++ src/components/TidesDisplay/TidesDisplay.js
@@ -9,6 +9,11 @@ import { connect } from 'react-redux';
 import RF from "react-native-responsive-fontsize";
 import moment from 'moment';
 
+const localDateTimeMachine = epoch => {
+  const myDate = new Date( epoch * 1000 );
+  return myDate.toLocaleString()
+}
+
 const mapStateToProps = state => {
   const { tideData } = state.search;
   return {
@@ -19,18 +24,48 @@ const mapStateToProps = state => {
 export class TideDisplay extends React.Component {
 
   render() {
-    const { tideData } = this.props;
+    let { tideData } = this.props;
     let tideDisplay = null;
+    let tides = null;
     if (tideData) {
-      let date = moment(tideData.date, 'MM DD YYYY').format('MMMM Do, YYYY');
-      tideDisplay = <View>
-        <View><Text style={{ textAlign: 'center', fontSize: RF(3), color: "white"}}>{tideData.city}, {tideData.state}</Text></View>
-        <View><Text style={{ textAlign: 'center', fontSize: RF(3), color: "white"}}>{date}</Text></View>
-      </View>
+      const { city, state } = tideData;
+       tides = tideData => {
+        tideData = tideData.tideData;
+        let currentDate = null;
+        const groupedTides = [];
+        for (let i = 0; i < tideData.length; i++) {
+          const tide = tideData[i];
+          
+          const thisDate =  moment(localDateTimeMachine(tide.dt).split(',')[0], 'MM Do, YYYY').format('YYYY MM DD');
+          if (currentDate === null || currentDate !== thisDate) {
+            currentDate = thisDate;
+            groupedTides.push([]);
+          }
+          groupedTides[groupedTides.length - 1].push(tide);
+        } 
+        return groupedTides.map((tidesArray, index) => {
+          let day;
+          day = localDateTimeMachine(tidesArray[0].dt).split(',')[0];
+          day = moment(day,'MM DD YYYY').format('dddd, MMMM Do');
+          return (
+            <View>
+              <Text>{day}</Text>
+              <View>{tidesArray.map((tide, i) => {
+                return <Text> key={i}>{tide.type} Tide at {moment(localDateTimeMachine(tide.dt).split(',')[1], 'h:mm a').format('h:mm a')}</Text>
+              })
+            }</View>
+            </View>
+          )
+        })
+      }
+      // tideDisplay = <View>
+      //   <View><Text style={{ textAlign: 'center', fontSize: RF(3), color: "white"}}>{city}, {state}</Text></View>
+      //   {tides}
+      // </View>
     }
     return (
       <View style={styles.container}>
-        {tideDisplay}
+        {tides}
       </View>
     )
   }
