42d3951a88b25acb1f93c3350f5bab50fe260c55
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2019-06-03 08:40:58 -0400

    fix android display

--- src/components/TidesDisplay/TidesDisplay.js
+++ src/components/TidesDisplay/TidesDisplay.js
@@ -12,8 +12,14 @@ import moment from 'moment';
 const uuid = require('uuid/v4');
 
 const localDateTimeMachine = epoch => {
-  const myDate = new Date( epoch * 1000 );
-  return myDate.toLocaleString()
+  let myDate = moment(epoch * 1000).format()
+  return myDate;
+}
+
+const localDate = epoch => {
+  let myDate = moment(epoch * 1000).format();
+  myDate = myDate.split('T')[0];
+  return myDate;
 }
 
 const mapStateToProps = state => {
@@ -25,14 +31,14 @@ const mapStateToProps = state => {
 };
 
 function makeTides(tideData) {
-  const { location } = tideData;
   const tideArray = tideData.tideData;
   let currentDate = null;
   const groupedTides = [];
   for (let i = 0; i < tideArray.length; i++) {
     const tide = tideArray[i];
     
-    const thisDate =  moment(localDateTimeMachine(tide.dt).split(',')[0], 'MM Do, YYYY').format('YYYY MM DD');
+    const thisDate =  localDate(tide.dt);
+
     if (currentDate === null || currentDate !== thisDate) {
       currentDate = thisDate;
       groupedTides.push([]);
@@ -41,8 +47,9 @@ function makeTides(tideData) {
   }
   days = groupedTides.map((tidesArray, index) => {
     let day;
-    day = localDateTimeMachine(tidesArray[0].dt).split(',')[0];
-    day = moment(day,'MM DD YYYY').format('dddd, MMMM Do');
+    day = localDate(tidesArray[0].dt)
+    console.log(day, 'DAY')
+    day = moment(day,'YYYY-MM-DD').format('dddd, MMMM Do');
     return (
       <View 
         key={uuid()} 
@@ -65,7 +72,7 @@ function makeTides(tideData) {
           }}
         >{day}</Text>
         <View key={uuid()}>{tidesArray.map((tide, i) => {
-          return <Text key={uuid()} style={{ fontSize: RF(3), marginBottom: hp('1%') }} >{tide.type} Tide at {moment(localDateTimeMachine(tide.dt).split(',')[1], 'h:mm a').format('h:mm a')}</Text>
+          return <Text key={uuid()} style={{ fontSize: RF(3), marginBottom: hp('1%') }} >{tide.type} Tide at {moment(localDateTimeMachine(tide.dt).split('T')[1], 'h:mm a').format('h:mm a')}</Text>
         })
       }</View>
       </View>
@@ -86,8 +93,8 @@ export class TideDisplay extends React.Component {
         <View style={{ margin: hp('2%'), padding: hp('1%'), borderRadius: 6, backgroundColor: 'rgba( 255, 255, 255, 0.8)'}}>
           <Text style={{ textAlign: 'center', fontSize: RF(4), fontWeight: '900' }}>{city}, {state}</Text>
         </View>
-        {tides}
-      </View>
+         {tides}
+        </View>
     } else if (error) {
       tideDisplay = <View style={{ margin: hp('4%'), padding: hp('1%'), borderRadius: 6, backgroundColor: 'rgba( 255, 255, 255, 0.8)'}}>
         <Text style={{ color: 'blue', textAlign: 'center', fontSize: RF(4), fontWeight: '900' }}>{error.message}</Text>
